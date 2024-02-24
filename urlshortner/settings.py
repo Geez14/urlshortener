b1ce99c21dacb908e5b3bff77f617a039cd2faa0
aster14 <91541128+Geez14@users.noreply.github.com>
2024-02-24 15:47:48 +0530

    Removed the heroku
    Signup not working for the heroku website!

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+# import django_heroku
+# import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,8 @@ SECRET_KEY = 'django-insecure-sv$lj_8$245)ehbbte*7+!gfl2wo$c7ei+uib71#8azxw!iu2o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# "*" == all the host
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -74,12 +78,18 @@ WSGI_APPLICATION = 'urlshortner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Old Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# # New Database! failed to dyploy heroku
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 
 # Password validation
